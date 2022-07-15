269611997fe5db0b9d750d1026e4ed8633333042
Dilan_105248 <dilan.deshapriya@axiatadigitallabs.com>
2022-07-15 23:31:58 +0530

    added gateway configurations added netflix eureka maven dependency

--- src/main/java/com/statistics/service/StatisticsServiceApplication.java
+++ src/main/java/com/statistics/service/StatisticsServiceApplication.java
@@ -2,8 +2,10 @@ package com.statistics.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StatisticsServiceApplication {
 
 	public static void main(String[] args) {
